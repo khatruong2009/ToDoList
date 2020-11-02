e156bb3c41d469e87f351fb79fe1dc9937a61f5a
khatruong2009 <khatruong2009@gmail.com>
2020-11-02 14:39:55 -0500

    restart to do list

---
+++ todo/src/App.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import './App.css';
+import ToDos from "./Components/ToDos";
+import Header from "./Components/Header";
+import Input from "./Components/Input";
+
+function App() {
+
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ todo/src/Components/ToDos.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ToDo from "./ToDo";
+
+const ToDos = (props) => {
+  return (
+    <div>
+      <ul>
+        
+      </ul>
+    </div>
+  )
+}
+
+export default ToDos;
